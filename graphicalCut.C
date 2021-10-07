9d8a459c83435713cc091de76f47d9d717f9e025
Ruchi Garg <ruchigarg@Ruchis-MacBook-Pro.local>
2021-10-07 18:56:41 -0400

    shf

--- graphicalCut.C
+++ graphicalCut.C
@@ -1,6 +1,6 @@
 #include "secarAnalysis.h"
 
-TCutG * graphicalCut(int Si[2], int dE[2], TString name){
+TCutG * graphicalCut(double Si[2], double dE[2], TString name){
   TCutG *cutg = new TCutG(name,5);
   cutg->SetPoint(0,Si[0],dE[0]);
   cutg->SetPoint(1,Si[1],dE[0]);
