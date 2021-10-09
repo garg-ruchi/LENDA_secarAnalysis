952995d06b2da5840e757da0e0a0f67f3433f690
Ruchi Garg <ruchigarg@Ruchis-MacBook-Pro.local>
2021-10-08 21:11:20 -0400

    uqef

--- sortLScintHits.C
+++ sortLScintHits.C
@@ -9,17 +9,11 @@ struct lscintDTO{
 lscintDTO sortLScintHits(int mult_LScint, vector<int> *energy_LScint, vector<int> *chan_LScint, vector<double> *time_LScint){
   lscintDTO lscintHit;
   if(mult_LScint>0 && mult_LScint<4){
-    cout << __LINE__ << endl;
     for(int i=0; i<mult_LScint; i++){
-      cout << __LINE__ << endl;
       lscintHit.energyCal.push_back((*energy_LScint)[i]);
-      cout << __LINE__ << endl;
       lscintHit.chan.push_back((*chan_LScint)[i]);
-      cout << __LINE__ << endl;
       lscintHit.time.push_back((*time_LScint)[i]);
-      cout << __LINE__ << endl;
     }
-    cout << __LINE__ << endl;
   }
   return lscintHit;
 }
